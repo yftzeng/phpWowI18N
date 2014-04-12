b3eb60487096c4df2d3f4fbf89a014e8bcc25a5f
Yi-Feng Tzeng <yftzeng@gmail.com>
2014-04-12 23:12:18 +0800

    fix sample code

--- samples/sample.php
+++ samples/sample.php
@@ -12,6 +12,12 @@ use Wow\I18N\WowI18N;
 
 $I = new WowI18N(I18N_LANGFILEPATH, I18N_LANGFILECACHEPATH, I18N_FALLBACKLANG, $forcedLang);
 
+echo $I->T(date('Y-m-d H:i:s'), 'UTC');
+echo "\n";
+echo $I->T(date('Y-m-d H:i:s'), 'Asia/Taipei');
+echo "\n";
+exit;
+
 // force set current lang
 $I->setCurrencyMinFractionDigits(5);
 $I->setCurrencyMaxFractionDigits(7);
@@ -27,12 +33,12 @@ $I->setTimeZone('America/New_York');
 $timezone = '2015-12-25 14:15:11';
 echo 'Current time: '.$timezone."\n";
 echo 'Current timezone: '.$I->T($timezone)."\n";
+echo 'Current timezone (UTC): '.$I->T($timezone, 'UTC')."\n";
+echo 'Current timezone (Taipei): '.$I->T($timezone, 'Asia/Taipei')."\n";
 $I->setTimeZone('UTC');
 echo 'Current timezone (UTC): '.$I->T($timezone)."\n";
-echo 'Current timezone (UTC): '.$I->T($timezone, 'UTC')."\n";
 $I->setTimeZone('Asia/Taipei');
-echo 'Current timezone (UTC): '.$I->T($timezone)."\n";
-echo 'Current timezone (UTC): '.$I->T($timezone, 'Asia/Taipei')."\n";
+echo 'Current timezone (Taipei): '.$I->T($timezone)."\n";
 
 // trans
 echo L::DEMO."\n";
